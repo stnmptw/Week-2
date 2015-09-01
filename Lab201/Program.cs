41268583a429a3c076b2232528373fac075911a1
STNMTPW <unknown-user@10.20.20.108>
2015-09-01 14:11:06 +0700

    Edit Student.cs  and Program.cs file Change to use get,set method

--- Lab201/Program.cs
+++ Lab201/Program.cs
@@ -23,7 +23,7 @@ namespace Lab2
 			Console.WriteLine ("\n--- Playing with s4, s5, s6 objects ---");
 			stds [4].Name = "Mario";
 			stds [5].YearOfBirth = 1900;
-			stds [6].isActive = false;
+			stds [6].IsActive = false;
 			for (int i = 4; i < stds.Length; i++)
 				Console.WriteLine (stds [i]);
 			
--- Lab201/Student.cs
+++ Lab201/Student.cs
@@ -4,65 +4,110 @@ namespace Lab2
 {
 	public class Student
 	{
-		public string Name;
-		public string id;
-		public int YearOfBirth;
-		public bool isActive;
+		private string name;
+		private string id;
+		private int yearOfBirth;
+		private bool isActive;
+
+		public string Name
+		{
+			set
+			{
+				name = value;
+			}
+			get
+			{
+				return name;
+			}
+		}
+		public string Id
+		{
+			set
+			{
+				id = value;
+			}
+			get
+			{ return id;}
+		}
+		public int YearOfBirth
+		{
+			set
+			{ 
+				if (value >=1950 && value <= 2000) {
+					yearOfBirth = DateTime.Now.Year - value;
+				} 
+				else {
+					Console.WriteLine ("{0} :error try setting invalid year-of-birth value!",id);
+
+				}
+			}
+			get
+			{
+				return yearOfBirth;
+
+			}
+		}
+
+		public bool IsActive
+		{
+			set
+			{
+				isActive = value;
+			}
+			get
+			{
+				if (isActive == true) {
+					
+					return isActive;
+				} else {
+					
+					return isActive;
+				}
+			}
+		}
 
 		public Student ()
 		{
 			Name = "John Doe";
-			id = "unknow";
+			Id = "unknow";
 			YearOfBirth = 1995;
-			isActive = false;
+			IsActive = false;
 
 		}
 		public Student(string n,string i)
 		{
 			Name = n;
-			id = i;
+			Id = i;
 			YearOfBirth = 1995;
-			isActive = true;
+			IsActive = true;
 
 		}
 		public Student(string n,string i,int y)
 		{
 			Name = n;
-			id = i;
+			Id = i;
 			YearOfBirth = y;
-			isActive = true;
+			IsActive = true;
 
 		}
 		public Student(string n,string i,int y,bool a)
 		{
 			Name = n;
-			id = i;
+			Id = i;
 			YearOfBirth = y;
-			isActive = a;
+			IsActive = a;
 
 		}
-		private string calculate(int x)
-		{
-			if (x >=1950 && x <= 2000) {
-				return Convert.ToString( DateTime.Now.Year - x);
-			} 
-			else {
-				Console.WriteLine ("{0} :error try setting invalid year-of-birth value!",id);
-				return "22";
-			}
 
-		}
-		private string getActive()
-		{
-			if (isActive == true) {
-				return "is active student";
-			} else {
-				return "is NOT active student";
-			}
-		}
+
 		public override string ToString ()
 		{
-			return string.Format ("[Student:{0},({1}), age = {2},{3}]",Name,id,calculate(YearOfBirth),getActive());
+			if (isActive) {
+				return string.Format ("[Student:{0},({1}), age = {2},is active student]",Name,Id,YearOfBirth);
+			} else {
+				return string.Format ("[Student:{0},({1}), age = {2},is NOT active student]",Name,Id,YearOfBirth);
+			}
+
 		}
 
 	}
